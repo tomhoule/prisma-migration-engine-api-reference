1da9dd35bf454807d0f2b6f8a8417ffc31cac3eb
Tom Houlé <tom@tomhoule.com>
2022-01-17 08:44:03 +0100

    Delete dead code

--- builder.sh
+++ builder.sh
@@ -3,21 +3,4 @@ echo $API_JSON | jq > $out/api.json
 
 export API_JSON=`echo $API_JSON | jq`
 
-# export API_METHOD_NAMES_JSON=`echo $API_JSON | jq '.methods | keys'`
-
-# echo $API_METHOD_NAMES_JSON > $out/mehodNames.json
-
-# export TEMPLATES_DIR=$src/templates
-
-# # First write the lib.rs
-# shab $src/templates/lib.rs.shab > $out/lib.rs
-
-# # Then write each module
-# echo $API_METHOD_NAMES_JSON | jq -r '.[]' | \
-#     while read methodName; do
-#         export METHOD=`echo $API_JSON | jq .methods.$methodName`;
-#         export METHOD_NAME=$methodName;
-#         shab $src/templates/method.rs.shab > $out/$methodName.rs
-#     done
-
 codegen
